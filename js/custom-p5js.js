ee1b766ef272d978197ccf147b2eee5cecba92b8
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2016-07-19 11:08:57 -0600

    add webgl torus shape

--- js/custom-p5js.js
+++ js/custom-p5js.js
@@ -3,17 +3,30 @@ function preload() {
 }
 
 function setup() {
-	var myCanvas = createCanvas(windowWidth, windowHeight - 5);
+	var myCanvas = createCanvas(windowWidth, windowHeight - 5, WEBGL);
+
+	/* use orthographic view */
+	ortho(-width, width, height, -height/2, 0.1, 100);
 
 	/* insert canvas inside #canvasWrap */
 	myCanvas.parent('canvasWrap');
 }
 
 function draw() {
-	if (mouseIsPressed) {
+
+	/* 3D WebGL */
+	background(255);
+
+	rotateX(frameCount * 0.01);
+	rotateY(frameCount * 0.01);
+	torus(200, 60);
+
+
+	/* 2d circles */
+	/*if (mouseIsPressed) {
 		fill(0);
 	} else {
 		fill(255);
 	}
-	ellipse(mouseX, mouseY, 80, 80);
+	ellipse(mouseX, mouseY, 80, 80);*/
 }
